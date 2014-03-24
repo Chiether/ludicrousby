467c45936942cb7b6451b7302f5e434b86989133
Norikaz Ishii <chiether+github@gmail.com>
2014-03-24 14:30:09 +0900

    split Array#cumulative example

--- ruby/array/cumulative.rb
+++ ruby/array/cumulative.rb
@@ -1,18 +1,20 @@
 class Array
-  # get/replace cumulative numbers.
+  # get/replace cumulative numbers. (not bang method)
   # @return [Array<Fixnum/Float>] number of before/after the decimal point.
-  # @example not bang method
+  # @example
   #  values = [1, 2, 3, 4, 5]
   #  values.cumulative #=> [1, 3, 6, 10, 15]
   #  values # => [1, 2, 3, 4, 5]
-  # @example bang method
-  #  values = [1, 2, 3, 4, 5]
-  #  values.cumulative! #=> [1, 3, 6, 10, 15]
-  #  values # => [1, 3, 6, 10, 15]
   def cumulative
     memo = 0
     return self.map{|i| memo+=i}
   end
+  # get/replace cumulative numbers. (bang method)
+  # @return [Array<Fixnum/Float>] number of before/after the decimal point.
+  # @example bang method
+  #  values = [1, 2, 3, 4, 5]
+  #  values.cumulative! #=> [1, 3, 6, 10, 15]
+  #  values # => [1, 3, 6, 10, 15]
   def cumulative!
     return self.replace self.cumulative
   end

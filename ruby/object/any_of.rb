e4c407641b77afbd72df3d7a0a5ef6508ed95143
Norikaz Ishii <chiether+github@gmail.com>
2014-03-24 13:47:11 +0900

    modified @example

--- ruby/object/any_of.rb
+++ ruby/object/any_of.rb
@@ -4,8 +4,8 @@ class Object
   # @return [Boolean] condition
   # @see https://bugs.ruby-lang.org/issues/9585 #in? has been started to feature's discussion.
   # @example
-  #  "sample".any_of? 1, 2, "sample"   # => true
-  #  "sample".any_of? [1, 2, "sample"] # => false
+  #  'sample'.any_of? 1, 2, 'sample'   # => true
+  #  'sample'.any_of? [1, 2, 'sample'] # => false
   #  (0.0).any_of? [0.0, 1, 2] #=> true
   #  (0.0).any_of? [0, 1, 2] # => false
   def any_of?(*params)

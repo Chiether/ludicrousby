36a95b8f2ea2360ce62e3d810a490081c6bb77b4
Norikaz Ishii <chiether+github@gmail.com>
2014-03-24 16:37:10 +0900

    modified yardoc

--- ruby/array/proportion.rb
+++ ruby/array/proportion.rb
@@ -1,13 +1,13 @@
 module Enumerable
   # get proportions.
+  # @param [Numeric, Symbol] Numeric:base. :max using max in values. :min using min in values.
   # @return [Array<Number>] converted proportion points (caution: returned float-class but not fixed.)
   # @note Support enumerable of Array, Hash, Range
   # @example
-  #  values = [1, 2, 3, 4, 5]
-  #  values.proportion(:max) #=> [0.2, 0.4, 0.6, 0.8, 1.0]
-  #  values.proportion(:min) #=> [1.0, 2.0, 3.0, 4.0, 5.0]
-  #  values.proportion(3) #=> [0.3333333333333333, 0.6666666666666666, 1.0, 1.3333333333333333, 1.6666666666666667]
-  def proportion(base)
+  #  [1, 2, 3, 4, 5].proportion(:max) #=> [0.2, 0.4, 0.6, 0.8, 1.0]
+  #  {:a=>3, :b=>2, :c=>4}.proportion(:min)  #=> {:a=>1.5, :b=>1.0, :c=>2.0}
+  #  (2..6).proportion(4) #=> [0.5, 0.75, 1.0, 1.25, 1.5]
+  def proportion(base=:max)
     hash_mode = self.kind_of?(Hash) ? true : false
     values = hash_mode ? self.values : self.to_a
     base = case base
